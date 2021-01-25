61f75ac9d8442e85cb0ac0a0219888966f1d057b
caye <cayetanog2000@gmail.com>
2021-01-25 19:18:31 +0100

    añadiendo tokens a peticiones

--- routes/lists.js
+++ routes/lists.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { listsController } from '../controllers/lists';
-
+import {token} from '../services/passport'
 
 
 const router = Router();
 
-router.get('/', listsController.allLists)
-router.get('/:id', listsController.allListsID)
+router.get('/',token(), listsController.allLists)
+router.get('/:id',token(), listsController.allListsID)
 
 
 export default router;
--- routes/songs.js
+++ routes/songs.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { songController } from '../controllers/songs';
-
+import {token} from '../services/passport'
 
 
 const router = Router();
 
-router.get('/', songController.allSongs)
-router.get('/:id', songController.allSongsID)
+router.get('/',token(), songController.allSongs)
+router.get('/:id',token(), songController.allSongsID)
 
 
 export default router;
